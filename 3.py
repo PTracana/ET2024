4b2f8843f1ceab11acbbbae862a6729351421705
Pedro Proença Tracana <pedro.tracana@tecnico.ulisboa.pt>
2024-05-02 17:29:49 +0100

    Refactor stats function in 3.py to include total_busy_time calculation

--- 3.py
+++ 3.py
@@ -21,29 +21,32 @@ def exponential_distribution(n,l):
         i -= 1
     return event_timestamp
 
-def stats(total_waiting_time, num_events, total_queue_time, total_time, idle, arrival_rate, service_rate):
+def stats( total_queue_time, total_processing_time,packets_served ,ending_timer,system_idle_time, arrival_rate, service_rate):
     
     rho = arrival_rate / service_rate
     Lq = rho**2 / (1 - rho)
     print("Theoretical Average Queue Size",Lq)
+    
     # falta calcular do experimental
     
+    total_waiting_time =  total_queue_time + total_processing_time
     print ("------------------------------------")
 
-    average_packet_time_in_system =  (total_waiting_time + total_queue_time)/num_events
+    average_packet_time_in_system =  total_waiting_time/packets_served if packets_served > 0 else 0
     print("Average packet time in system: ", average_packet_time_in_system)
     Ws = 1 / (service_rate - arrival_rate)
     print("Theoretical Average Packet Time in the system: ", Ws)
     
     print ("------------------------------------")
-    average_packet_time_in_queue = total_waiting_time/num_events
+    average_packet_time_in_queue = total_queue_time / packets_served if packets_served > 0 else 0
     print("Average packet waiting time: ", average_packet_time_in_queue)
     Wq = Ws - (1 / service_rate)
     print("Theoretical Average Packet Time in the queue",Wq)
 
     #falta o teorico
-    total_busy_time = (total_time - idle) / total_time *100
-  #  print("Total busy time: ", total_busy_time)
+    print ("------------------------------------")
+    total_busy_time =  (ending_timer - system_idle_time)/ending_timer
+    print("Total busy time: ", total_busy_time)
     
     return
 
@@ -53,77 +56,85 @@ def processing(arrival_rate, service_rate, event_cap):
     server_free = True
   
 
-    total_waiting_time = 0
-    total_time_in_system = 0
+    total_queue_time = float(0)
+    total_processing_time = float(0)
+    system_idle_time = float(0)
+    packet_arrived = 0
+    packets_served = 0
+    block_in_creation = False
     
-    total_queue_time = 0
-    num_events = 1
     
     #
-    idle = 0
     timer = 0
     event_list.append((0,"in"))
     
-    idle_since = 0
+    idle_since = float(0)
     
     while event_list:
         # "reading" packet arrival step 1
         current_event = event_list.pop(0)
     
         #step 2
-        if current_event[1] == "in":
+        if current_event[1] == "in" and not block_in_creation:
             # add tuple to list
-            queue.put(current_event)
             
             # new event generation
-            next_event = (current_event[0] + exponential_distribution(1,arrival_rate)[0], "in")
-            event_list.append(next_event)
-            sorted(event_list, key=lambda x: x[0])
+            if packet_arrived <= event_cap:
+                queue.put(current_event)
+                next_event = (current_event[0] + exponential_distribution(1,arrival_rate)[0], "in")
+                event_list.append(next_event)
+                packet_arrived += 1
+                sorted(event_list, key=lambda x: x[0])
             #jump to 4
+            else: 
+                block_in_creation = True
+                continue
             
         #step 3 
         else:
             # else mark server as free and count the waiting time
             server_free = True
-            num_events += 1
+            idle_since = current_event[0]
+            packets_served += 1
             
-            
-            #idle_since = arriving_event[0]
-            
-            #total_waiting_time += arriving_event[0] - timer 
+
             
         #step 4
         # server is free queue is empty 
         if (server_free and queue.empty()):
-            #timer = arriving_event[0]
+            timer = current_event[0]
             continue
-        else:  
-            leaving_event = queue.get()
-            server_free = False
+ 
+        if not server_free:
+            timer = current_event[0]
+            continue
         
-            #if idle_since != 0:
-                #idle += leaving_event[0] - idle_since
-                #idle_since = 0
-                
-                
-            # add to the event list
-            event_list.append((leaving_event[0] + exponential_distribution(1,service_rate)[0], "out"))
-            sorted(event_list, key=lambda x: x[0])
+        leaving_event = queue.get()
+        server_free = False
+    
+        if idle_since is not None:
+            system_idle_time += leaving_event[0] - idle_since
+            idle_since = None
             
             
-            #total_queue_time += arriving_event[0]- timer
-            #total_waiting_time += arriving_event[0] - timer
-            #timer = arriving_event[0]
+        # add to the event list (é preciso guardar o timestamp para manter o tempo de processamento)
+        time_in_processing = current_event[0] + exponential_distribution(1,service_rate)[0]
+        event_list.append((time_in_processing, "out"))
+        event_list = sorted(event_list, key=lambda x: x[0])
+        
+        
+        total_queue_time += current_event[0]- leaving_event[0]
+        total_processing_time += time_in_processing - current_event[0]
+        timer = current_event[0]
 
         
         #num events is over event cap
-        if num_events >= event_cap:
+        if timer > event_cap and packets_served == packet_arrived:
             break
-        
-        
-    total_time = timer
+            
+    ending_timer = timer
     
-    stats(total_waiting_time, num_events, total_queue_time, total_time, idle, arrival_rate, service_rate)
+    stats(total_queue_time, total_processing_time, packets_served, ending_timer, system_idle_time, arrival_rate, service_rate)
     return 
     
 
